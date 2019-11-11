92b3cf729411494af0649983541f02bb949f5fe1
Paul Vaneveld <paul.vaneveld@valtech.com>
2019-11-11 11:41:27 +0100

    fixed post css issue

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,14 @@
 /* eslint-disable */
 const postcssPresetEnv = require(`postcss-preset-env`)
+const postcssCustomMedia = require('postcss-custom-media');
+const postcssImport = require("postcss-import");
+
 module.exports = () => ({
   plugins: [
-    postcssPresetEnv({
-      stage: 0,
-    }),
+    postcssImport(),
+    postcssPresetEnv(),
+    postcssCustomMedia({
+        importFrom: './src/styles/00-variables/variables.css'
+    })
   ],
 })
