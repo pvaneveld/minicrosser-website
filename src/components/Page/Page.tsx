7838c41f05a4aafd39f2d0926b3d962e2c4c0120
Paul Vaneveld <paul.vaneveld@valtech.com>
2019-11-18 09:20:19 +0100

    responseive image in progress

--- src/components/Page/Page.tsx
+++ src/components/Page/Page.tsx
@@ -2,18 +2,41 @@ import React from 'react';
 import { ReactNode } from 'react';
 import style from './Page.module.css';
 import mockImage from '../../img/mock-image.jpg';
+import { graphql, useStaticQuery } from 'gatsby';
+import Img, { FluidObject } from 'gatsby-image';
 
 interface PageProps {
   children?: ReactNode;
   id: string;
   firstPage: boolean;
   lastPage: boolean;
+  data: {
+    file: {
+      childImageSharp: {
+        fluid: FluidObject;
+      };
+    };
+  };
 }
 
 const Page: React.SFC<PageProps> = props => {
+  const pageQuery = useStaticQuery(graphql`
+    query {
+      file(absolutePath: { regex: "/mock-image/" }) {
+        childImageSharp {
+          # Specify a fluid image and fragment
+          # The default maxWidth is 800 pixels
+          fluid(maxWidth: 1200) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `);
+
   return (
     <div id={props.id} className={style.page}>
-      <img src={mockImage} alt="mock" className={style.image} />
+      <Img fluid={pageQuery.file.childImageSharp.fluid} alt="" className={style.image} />
       <div
         className={`${style.pageContent} ${props.firstPage ? style.firstPage : ''} ${
           props.lastPage ? style.lastPage : ''
