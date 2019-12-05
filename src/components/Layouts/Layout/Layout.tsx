990a739bb7ef430d6282498df9e6800cbae60976
Paul Vaneveld <paul.vaneveld@valtech.com>
2019-12-05 20:01:06 +0100

    lowercase flag

--- src/components/Layouts/Layout/Layout.tsx
+++ src/components/Layouts/Layout/Layout.tsx
@@ -5,7 +5,7 @@ import 'reset-css';
 import '../../../styles/global.css';
 import Header from '../../Header/Header';
 import Footer from '../../Footer/Footer';
-import Flag from '../../../icons/Flag.svg';
+import Flag from '../../../icons/flag.svg';
 import style from './Layout.module.css';
 
 interface LayoutProps {
