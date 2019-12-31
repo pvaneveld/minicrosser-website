6a8964f1c4d61cfec5ba3447a80b09c5ce471a61
Paul Vaneveld <paul.vaneveld@gmail.com>
2019-12-31 12:11:41 +0100

    changed duplicate title

---
+++ src/pages/configureren.md
@@ -0,0 +1,4 @@
+---
+templateKey: configurator
+title: Disclaimer
+---
---
+++ src/templates/configurator.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Layout from '../components/Layouts/Layout/Layout';
+import Page from '../components/SmoothScroll/Page/Page';
+
+const Configurator: React.SFC = () => {
+  return (
+    <Layout theme={{ headerDark: true, footerDark: true }}>
+      <Page>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Adipisci, obcaecati enim incidunt, error perspiciatis
+        dolorem voluptas expedita voluptate harum sunt aut sapiente? Harum deleniti quos sint nisi, repudiandae nesciunt
+        quas!
+      </Page>
+    </Layout>
+  );
+};
+
+export default Configurator;
