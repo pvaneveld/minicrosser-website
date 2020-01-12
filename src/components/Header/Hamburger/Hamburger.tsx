daec95e578ce98f8a0cecbfaffa652af0edd1efe
Paul Vaneveld <paul.vaneveld@gmail.com>
2020-01-12 12:37:04 +0100

    changed up some heights

--- src/components/Header/Hamburger/Hamburger.tsx
+++ src/components/Header/Hamburger/Hamburger.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import style from './Hamburger.module.css';
 
 interface HamburgerProps {
@@ -6,16 +6,24 @@ interface HamburgerProps {
   clickHandler: (event: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
-const Hamburger: React.SFC<HamburgerProps> = props => (
-  <button
-    onClick={props.clickHandler}
-    className={`${style.hamburger} ${style.hamburgerSpring}${props.isActive ? ` ${style.isActive}` : ''}`}
-    type="button"
-  >
-    <span className={style.hamburgerBox}>
-      <span className={style.hamburgerInner}></span>
-    </span>
-  </button>
-);
+const Hamburger: React.SFC<HamburgerProps> = props => {
+  useEffect(() => {
+    const overflow = props.isActive ? 'hidden' : 'auto';
+
+    document.body.setAttribute('style', `overflow-y: ${overflow}`);
+  }, [props.isActive]);
+
+  return (
+    <button
+      onClick={props.clickHandler}
+      className={`${style.hamburger} ${style.hamburgerSpring}${props.isActive ? ` ${style.isActive}` : ''}`}
+      type="button"
+    >
+      <span className={style.hamburgerBox}>
+        <span className={style.hamburgerInner}></span>
+      </span>
+    </button>
+  );
+};
 
 export default React.memo(Hamburger);
